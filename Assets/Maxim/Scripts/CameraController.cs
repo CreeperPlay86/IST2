8a86ef64d266a41f88b0979e38c557a6a33ba199
Haedus <creeperplay86@gmail.com>
2025-02-09 12:46:42 +0500

    1.0.2

--- Assets/Maxim/Scripts/CameraController.cs
+++ Assets/Maxim/Scripts/CameraController.cs
@@ -1,37 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float zoomSpeed = 0.5f;
+    public float zoomSpeed = 1f; //Скорость Приближения и Отдаления
     
     public CinemachineFreeLook FreeLook;
+
+    public float ySpeed = 2f;
+    public float xSpeed = 300f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
     void Update()
+    
     {
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1)) //Проверка Зажатия ПКМ
         {
-            FreeLook.enabled = true;
+            
+            FreeLook.m_XAxis.m_MaxSpeed = xSpeed;
+            FreeLook.m_YAxis.m_MaxSpeed = ySpeed;
+            
         }
         else
         {
-            FreeLook.enabled = false;
+            FreeLook.m_XAxis.m_MaxSpeed = 0f;
+            FreeLook.m_YAxis.m_MaxSpeed = 0f;
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        if (FreeLook.m_Orbits[0].m_Radius < 2f) //Блокировка Приближения
         {
+            FreeLook.m_Orbits[0].m_Radius = 2;
+            FreeLook.m_Orbits[1].m_Radius = 2;
+            FreeLook.m_Orbits[2].m_Radius = 2;
+        }
+
+        if (FreeLook.m_Orbits[0].m_Radius > 10f) //Блокировка Дальности
+        {
+            FreeLook.m_Orbits[0].m_Radius = 10;
+            FreeLook.m_Orbits[1].m_Radius = 10;
+            FreeLook.m_Orbits[2].m_Radius = 10;
+        }
+        
+        if (Input.GetAxis("Mouse ScrollWheel") != 0) //Приближение и Отдаление в зависимости от прокрутки колеса мыши
+        {
+            
             FreeLook.m_Orbits[0].m_Radius += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
             FreeLook.m_Orbits[1].m_Radius += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
             FreeLook.m_Orbits[2].m_Radius += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+            
         }
+        
     }
 }
